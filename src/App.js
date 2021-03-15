f99b0b09385c061957947a501355fe1ebc355a9e
LaToddra Belton <latoddra.belton@codeimmersives.com>
2021-03-15 09:42:48 -0500

    seacrh button loading

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import logo from './logo.svg';
+
+import MainPage from './container/MainPage'
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-    <h1>hi</h1>
-    </div>
-  );
+
+    return (
+      <div className="App">
+      <BrowserRouter>
+      <MainPage />
+      </BrowserRouter>
+      </div>
+    );
 }
 
 export default App;
