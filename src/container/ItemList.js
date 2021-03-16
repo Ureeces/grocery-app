3c02d33c259669bb731403c26dca66defd68c70a
LaToddra Belton <latoddra.belton@codeimmersives.com>
2021-03-16 08:33:35 -0500

    adding classes to make styling easier

--- src/container/ItemList.js
+++ src/container/ItemList.js
@@ -1,54 +1,59 @@
-import React, { Component } from "react";
+import React, { Component} from "react";
 import { v4 as uuidv4 } from "uuid";
 import AddItem from "../components/AddItem";
 import EditItem from "../components/EditItem";
+import Banner from '../components/Banner'
+import './ItemList.css'
+// import Filter  from '../components/Filter';
+// import Search from '../components/Search';
 
 export default class ItemList extends Component {
   state = {
     // The list of items in the store
+    
     itemList: [
       {
         id: uuidv4(),
         name: "Martin Portable Propane BBQ Gas Grill",
         price: "139.99",
         imageLink:
-          "https://images-na.ssl-images-amazon.com/images/I/71OpFNffgcL._AC_SL1500_.jpg",
+        "https://images-na.ssl-images-amazon.com/images/I/71OpFNffgcL._AC_SL1500_.jpg",
         description:
-          "Compatible with both high and low pressure lines – ideal for trailers – this portable gas grill is a must-have for any barbeque lover. Not only does it need no external power source (thanks to its piezo-ignition system) it gives you the control over how each and every meal turns out. That’s why we created the MARTIN portable propane bbq gas grill, you have the freedom to adjust the temperature according to the meat – or vegetable – you’re grilling which means you’ll be able to get that delicious “seared on the outside, juicy on the inside” performance every time. Thanks to the closing lid you’ll find it’s much easier to get evenly-cooked food and keep the bugs away while you’re cooking.",
+        "Compatible with both high and low pressure lines – ideal for trailers – this portable gas grill is a must-have for any barbeque lover. Not only does it need no external power source (thanks to its piezo-ignition system) it gives you the control over how each and every meal turns out. That’s why we created the MARTIN portable propane bbq gas grill, you have the freedom to adjust the temperature according to the meat – or vegetable – you’re grilling which means you’ll be able to get that delicious “seared on the outside, juicy on the inside” performance every time. Thanks to the closing lid you’ll find it’s much easier to get evenly-cooked food and keep the bugs away while you’re cooking.",
       },
       {
         id: uuidv4(),
         name: "PlayStation 5 (digital)",
         price: "500.00",
         imageLink:
-          "https://static.highsnobiety.com/thumbor/Hg2jNcXWBvnB4YA2YMhuHOGN-V4=/1600x1067/static.highsnobiety.com/wp-content/uploads/2020/06/16214518/playstation-5-price-leak-01.jpg",
+        "https://static.highsnobiety.com/thumbor/Hg2jNcXWBvnB4YA2YMhuHOGN-V4=/1600x1067/static.highsnobiety.com/wp-content/uploads/2020/06/16214518/playstation-5-price-leak-01.jpg",
         description:
-          "Experience lightning-fast loading with an ultra-high speed SSD, deeper immersion with support for haptic feedback, adaptive triggers and 3D Audio*, and an all-new generation of incredible PlayStation® games.",
+        "Experience lightning-fast loading with an ultra-high speed SSD, deeper immersion with support for haptic feedback, adaptive triggers and 3D Audio*, and an all-new generation of incredible PlayStation® games.",
       },
     ],
-
+    
     // These are for the Add Component - contain the variables for a new object
     itemName: "",
     itemPrice: "",
     itemImageLink: "",
     itemDescription: "",
-
+    
     // Toggle Display of Add Component
     addDisplayOn: false,
-
+    
     // Toggle Display of Edit Component
     editDisplayOn: false,
-
+    
     // The ID of the item to edit
     selectedItemID: "",
-
+    
     // These are for the Edit Component - contain the variables for the editing object
     tempName: "",
     tempPrice: "",
     tempImageLink: "",
     tempDescription: "",
   };
-
+  
   /****************************************************************
    * Functions for Deleting Item
    ***************************************************************/
@@ -57,13 +62,14 @@ export default class ItemList extends Component {
     const newItemList = this.state.itemList.filter((item) => {
       return item.id !== targetID;
     });
-
+    
     this.setState({
       ...this.state,
       itemList: newItemList,
     });
   };
-
+  
+  
   /****************************************************************
    * Functions for Adding Item
    ***************************************************************/
@@ -141,7 +147,10 @@ export default class ItemList extends Component {
       };
     });
   };
-
+ /****************************************************************
+   * Functions for Searching Item
+   ***************************************************************/
+ 
   /****************************************************************
    * On Change Handlers
    ***************************************************************/
@@ -160,14 +169,19 @@ export default class ItemList extends Component {
     });
   };
 
+  //Search OnChange function
+  
+
   /****************************************************************
    * Rendering
    ****************************************************************/
   render() {
     return (
       <div>
+        {/* Banner */}
+      <Banner />
         {/* For Add Display */}
-        <button onClick={this.toggleAddDisplay}>Add Item</button>
+        <button className="add-button" onClick={this.toggleAddDisplay}>Add Item</button>
         {this.state.addDisplayOn ? (
           <AddItem
             itemList={this.state.itemList}
@@ -180,7 +194,7 @@ export default class ItemList extends Component {
           />
         ) : null}{" "}
         <br />
-        <select onChange={this.handleEditSelect}>
+        <select class="selector" onChange={this.handleEditSelect}>
           <option selected disabled>
             Select an Item to Edit
           </option>
@@ -204,13 +218,19 @@ export default class ItemList extends Component {
         {this.state.itemList.map(
           ({ name, price, imageLink, description, id }) => {
             return (
-              <div>
+              <div class ="products products-table">
+                <div class="product">
+
                 <button onClick={() => this.deleteItemHandle(id)}>X</button>
-                <p>Name: {name}</p>
-                <p>Price: ${price}</p>
-                <img src={imageLink} alt={`${name}`} width="600px" />
-                <p>{description}</p>
-                <hr></hr>
+                <img class="product-img"src={imageLink} alt={`${name}`} />
+                </div>
+                <div class="product-content">
+
+                <h3> {name}</h3>
+                <small>{description}</small>
+                <p class="product-text price">Price: ${price}</p>
+                </div>
+                {/* <hr></hr> */}
               </div>
             );
           }
